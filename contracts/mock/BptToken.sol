11c85e7cd5711178cd1f0bf422b022e88ee97d34
maxKMyt <maksim.korotkikh@protofire.io>
2023-09-11 18:13:27 +0300

    Add mock erc20 tokens

--- contracts/mock/BptToken.sol
+++ contracts/mock/BptToken.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.10;
 // pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract TestToken is ERC20 {
-    constructor() ERC20('Token1', 'Symbl1') {}
+
+contract BPTToken is ERC20 {
+    constructor() ERC20('BPTToken1', 'BPT1') {}
 
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
