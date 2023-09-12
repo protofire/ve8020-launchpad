4cd58b99ebfe40e8315ddc829d945c4621232dbb
maxKMyt <maksim.korotkikh@protofire.io>
2023-09-12 22:46:15 +0300

    Launchpad tests

--- test/launchpad.test.ts
+++ test/launchpad.test.ts
@@ -8,6 +8,7 @@ import {
   BigNumber,
   utils,
   constants,
+  ContractReceipt
 } from "ethers";
 
 import {
@@ -18,15 +19,16 @@ import {
   BPTToken,
 } from "../typechain-types";
 
-function dataFromEvent(event: any): string {
-  const requestKey = event.data.slice(66);
-  return `0x${requestKey}`;
-}
 
 let owner: Signer;
 let creator: Signer;
 let user1: Signer;
 let user2: Signer;
+
+let ownerAddress: string;
+let creatorAddress: string;
+let user1Address: string;
+let user2Address: string;
 
 let erc20Factory: ContractFactory;
 let rewardToken: TestToken;
@@ -45,6 +47,12 @@ describe("Launchpad", function () {
 
   before(async () => {
     [owner, creator, user1, user2] = await ethers.getSigners();
+    [ownerAddress, creatorAddress, user1Address, user2Address] = await Promise.all([
+      owner.getAddress(),
+      creator.getAddress(),
+      user1.getAddress(),
+      user2.getAddress(),
+    ]);
 
     erc20Factory = await ethers.getContractFactory('TestToken');
     rewardToken = (await erc20Factory.deploy()) as TestToken;
@@ -58,82 +66,174 @@ describe("Launchpad", function () {
     rdFactory = await ethers.getContractFactory('RewardDistributor');
     rewardDistributorImpl = (await rdFactory.deploy()) as RewardDistributor;
 
-    // await rewardToken.mi
+    await rewardToken.mint(creatorAddress, utils.parseEther("2000"));
   });
 
-  describe("State of deployment implementations", function () {
+  describe('Initial states', function() {
+    it('Should deploy mock reward token', async () => {
+      const name = await rewardToken.name();
+      const symbol = await rewardToken.symbol();
+      expect(name).to.equal('Token1');
+      expect(symbol).to.equal('Symbl1');
+    });
+
+    it('Should deploy mock BPT token', async () => {
+      const name = await bptToken.name();
+      const symbol = await bptToken.symbol();
+      expect(name).to.equal('BPTToken1');
+      expect(symbol).to.equal('BPT1');
+    });
+
+    it('Should deploy empty VE implementation', async () => {
+      const name = await votingEscrowImpl.name();
+      const symbol = await votingEscrowImpl.symbol();
+      const decimals = await votingEscrowImpl.decimals();
+      const someValue = await votingEscrowImpl.get_last_user_slope(user1Address);
+      const isInitialized = await votingEscrowImpl.is_initialized();
+      expect(name).to.equal('');
+      expect(symbol).to.equal('');
+      expect(decimals).to.equal(0);
+      expect(someValue).to.equal(0);
+      expect(isInitialized).to.equal(false);
+    });
+
+    it('Should deploy empty RewardDistributor implementation', async () => {
+      const ve = await rewardDistributorImpl.getVotingEscrow();
+      const timeCursor = await rewardDistributorImpl.getTimeCursor();    
+      const isInitialized = await rewardDistributorImpl.isInitialized();
+      expect(ve).to.equal(constants.AddressZero);
+      expect(timeCursor).to.equal(0);
+      expect(isInitialized).to.equal(false);
+    });
+
+  });
 
 
-    it("Should set the right unlockTime", async function () {
-      const { token, owner } = await loadFixture(deployToken);
-      const TokenFactory = await ethers.getContractFactory("TestToken");
-      const token2 = await TokenFactory.deploy();
-      console.log('token', token.address);
-      console.log('token2', token2.address);
+  describe('Deploy Launchpad constraints', function () {
+    it('Should be unable to deploy launchpad with zero VE address', async () => {
+      launchpadFactory = await ethers.getContractFactory('Launchpad');
 
+      await expect(launchpadFactory.deploy(
+        constants.AddressZero,
+        rewardDistributorImpl.address
+        )).to.be.rejectedWith('zero address');
+    });
 
-      expect(await token.decimals()).to.equal(18);
-      console.log('done');
+    it('Should be unable to deploy launchpad with zero rewardDistributor address', async () => {
+      launchpadFactory = await ethers.getContractFactory('Launchpad');
 
-      const name: string = 'VotingEscrow';
-      const symbol: string = 'VE';
+      await expect(launchpadFactory.deploy(
+        votingEscrowImpl.address,
+        constants.AddressZero
+        )).to.be.rejectedWith('zero address');
+    });
 
-      console.log('before factory');
-      const VotingEscrowFactory = await ethers.getContractFactory("VotingEscrow");
-      console.log('after factory');
-      const veImpl = await VotingEscrowFactory.deploy();
+    it('Should be unable to deploy launchpad with both zero addresses', async () => {
+      launchpadFactory = await ethers.getContractFactory('Launchpad');
 
-      
-      const RewardDistributorFactory = await ethers.getContractFactory("RewardDistributor");
-      const rewardDistributorImpl = await RewardDistributorFactory.deploy();
-
-
-      console.log('deploy ve factory');
-      const VEFactory = await ethers.getContractFactory("Launchpad");
-      const launchpad = await VEFactory.deploy(veImpl.address, rewardDistributorImpl.address);
-      console.log('launchpad', launchpad.address)
-      console.log('launchpad.ve', await launchpad.votingEscrow())
-      console.log('launchpad.rd', await launchpad.rewardDistributor())
-
-
-      console.log('launch:');
-      const txResult = await launchpad.deploy1(token2.address, 'VE1name', 'VE1symbol', 1694349020+999900);
-      let txReceipt = await txResult.wait();
-      console.log('events: ', txReceipt.events[0].args);
-      const ve1 = await ethers.getContractAt('VotingEscrow', '0x856e4424f806D16E8CBC702B3c0F2ede5468eae5')
-      const rd1 = await ethers.getContractAt('RewardDistributor', '0xb0279Db6a2F1E01fbC8483FCCef0Be2bC6299cC3')
-      console.log('ve1.name', await ve1.name());
-      console.log('rd1.ve:', await rd1.getVotingEscrow());
-
-      const txResult2 = await launchpad.deploy1(token2.address, 'VE2name', 'VE2symbol', 1694349020+999900);
-      let txReceipt2 = await txResult2.wait();
-      console.log('events: ', txReceipt2.events[0].args);
-      const ve2 = await ethers.getContractAt('VotingEscrow', '0x3dE2Da43d4c1B137E385F36b400507c1A24401f8')
-      const rd2 = await ethers.getContractAt('RewardDistributor', '0xddEA3d67503164326F90F53CFD1705b90Ed1312D')
-      console.log('ve2.name', await ve2.name());
-      console.log('rd2.ve:', await rd2.getVotingEscrow());
-
-
-      // const txResult11 = await launchpad.deploy11(token.address);
-      // let txReceipt11 = await txResult11.wait();
-      // console.log('events: ', txReceipt11.events[0].args);
-      // const tokenCopy = await ethers.getContractAt('TestToken', '0x9409fcA4c8899dc5Ec1142E24C4136F3B79Ef226')
-      // console.log('ve1.name', await tokenCopy.name())
-
-
-      // let events = txReceipt.events?.filter(
-      //   (event: any) => event.topics[0] === createIncreasePositionEvent
-      // );
-
-
-
-      // console.log('launch:');
-      // const txResult = await launchpad.deploy2(token2.address);
-      // console.log('txResult', txResult)
-      // let txReceipt = await txResult.wait();
-      // console.log('events: ', txReceipt.events);
+      await expect(launchpadFactory.deploy(
+        constants.AddressZero,
+        constants.AddressZero
+        )).to.be.revertedWith('zero address');
     });
   });
 
+  describe('Deploy Launchpad', function () {
+    before(async () => {
+      launchpadFactory = await ethers.getContractFactory('Launchpad');
+      launchpad = (await launchpadFactory.deploy(
+        votingEscrowImpl.address,
+        rewardDistributorImpl.address
+        )) as Launchpad;
+    });
+
+    it('Should set admin of launchpad', async () => {
+      expect(await launchpad.admin()).to.equal(ownerAddress);
+    });
+    
+    it('Should set correct VE implementation of launchpad', async () => {
+      expect(await launchpad.votingEscrow())
+        .to.equal(votingEscrowImpl.address);
+    });
+
+    it('Should set correct RD implementation of launchpad', async () => {
+      expect(await launchpad.rewardDistributor())
+        .to.equal(rewardDistributorImpl.address);
+    });
+  });
+
+  describe('Deploy VE system constraints', function () {
+    let name = 'MockName1';
+    let symbol = 'MockSymbol1';
+    
+    it('Should fail to create VE-System with incorrect token', async () => {
+      const rewardStartTime = (await time.latest()) + 10000000;
+
+      await expect(launchpad.deploy(
+        rewardDistributorImpl.address,
+        name,
+        symbol,
+        rewardStartTime
+        )).to.be.reverted;
+    });
+
+    it('Should fail to create VE-System with incorrect reward startTime (0)', async () => {
+      const rewardStartTime = 0;
+      
+      await expect(launchpad.deploy(
+        bptToken.address,
+        name,
+        symbol,
+        rewardStartTime
+        )).to.be.revertedWith('Cannot start before current week');
+    });
+
+    it('Should fail to create VE-System with incorrect reward startTime (current)', async () => {
+      let rewardStartTime = (await time.latest());
+      
+      await expect(launchpad.deploy(
+        bptToken.address,
+        name,
+        symbol,
+        rewardStartTime
+        )).to.be.revertedWith('Zero total supply results in lost tokens');
+    });
+  });
+
+  describe('Deploy VE system', function () {
+    let name = 'MockName1';
+    let symbol = 'MockSymbol1';
+    let txResult: ContractTransaction;
+    let txReceipt: ContractReceipt;
+
+    let votingEscrow: VotingEscrow;
+    let rewardDistributor: RewardDistributor;
+
+    before(async () => {
+      const rewardStartTime = (await time.latest()) + 10000000;
+      txResult = await launchpad.connect(creator).deploy(
+        bptToken.address,
+        name,
+        symbol,
+        rewardStartTime
+      );
+      txReceipt = await txResult.wait();
+    });
+
+    it("Should emit event on deployment", async () => {
+      // @ts-ignore
+      const event = txReceipt.events[0];
+      // @ts-ignore
+      expect(event.args.token).to.equal(bptToken.address);
+      // @ts-ignore
+      expect(event.args.admin).to.equal(creatorAddress);
+      // @ts-ignore
+      expect(event.args.votingEscrow)
+        .to.not.equal(constants.AddressZero);
+      // @ts-ignore
+      expect(event.args.rewardDistributor)
+        .to.not.equal(constants.AddressZero);
+    });
+  });
 
 });
