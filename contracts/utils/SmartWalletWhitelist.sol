2c4dc8431921a912663f2fc1bfa5627410e447ba
maxKMyt <maksim.korotkikh@protofire.io>
2023-10-16 15:35:23 +0300

    Refactor smartWalletChecker, update deployment scripts

---
+++ contracts/utils/SmartWalletWhitelist.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+interface SmartWalletChecker {
+    function check(address) external view returns (bool);
+}
+
+contract SmartWalletWhitelist {
+
+    address public immutable admin;
+
+    mapping(address => bool) public wallets;
+
+    address public checker;
+    bool public isAllowAll = false;
+
+    event ApproveWallet(address);
+    event RevokeWallet(address);
+    event NewChecker(address);
+    event SetAllowAll(bool);
+
+    modifier onlyAdmin() {
+        require(admin == msg.sender, "!admin");
+        _;
+    }
+
+    constructor(address _admin) {
+        admin = _admin;
+    }
+
+    // @notice Sets allowance (permission) for all contracts
+    // @param _isAllowAll - The boolean parameter to set if it allowed for all contract
+    function setAllowAll(bool _isAllowAll) external onlyAdmin {
+        isAllowAll = _isAllowAll;
+        emit SetAllowAll(_isAllowAll);
+    }
+
+    // @notice Sets checker contract for further allowance check
+    // @dev can be zero-address in case when no need to use `checker`
+    // @param _checker - The address of a checker contract
+    function setChecker(address _checker) external onlyAdmin {
+        checker = _checker;
+
+        emit NewChecker(_checker);
+    }
+
+    // @notice Approves one particular address
+    // @param _wallet - The address of a contract to be approved
+    function approveWallet(address _wallet) public onlyAdmin {
+        wallets[_wallet] = true;
+        
+        emit ApproveWallet(_wallet);
+    }
+
+    // @notice Approves list of addresses
+    // @param _wallets - The array of contract addresses to be approved
+    function approveWalletList(address[] calldata _wallets) external {
+        uint256 len = _wallets.length;
+        for (uint256 i; i < len; ) {
+            approveWallet(_wallets[i]);
+            unchecked { ++i; }
+        }
+    }
+
+    // @notice Revokes allowance for one particular address
+    // @param _wallets - The address of a contract to revoke allowance
+    function revokeWallet(address _wallet) public onlyAdmin {
+        require(msg.sender == admin, "!admin");
+        wallets[_wallet] = false;
+        
+        emit RevokeWallet(_wallet);
+    }
+
+    // @notice Revokes allowance for the list of addresses
+    // @param _wallets - The array of contract addresses to revoke allowance
+    function revokeWalletList(address[] calldata _wallets) external {
+        uint256 len = _wallets.length;
+        for (uint256 i; i < len; ) {
+            revokeWallet(_wallets[i]);
+            unchecked { ++i; }
+        }
+    }
+
+    // @notice Checks provided address for allowance or delegates such check to 'checker' contract (if exist)
+    // @param _wallet - The address to check for allowance
+    function check(address _wallet) external view returns (bool) {
+        if (isAllowAll) return true;
+
+        bool _check = wallets[_wallet];
+        if (_check) {
+            return _check;
+        } else {
+            if (checker != address(0)) {
+                return SmartWalletChecker(checker).check(_wallet);
+            }
+        }
+        return false;
+    }
+}
--- docs/misc_docs/SmartWalletWhitelist.md
+++ docs/misc_docs/SmartWalletWhitelist.md
@@ -12,24 +12,40 @@ Don't forget to specify desired networkName (full list of network names can be f
 After deploying the `SmartWalletWhitelist` smart contract, you need to add its address to the VotingEscrow contract. To do this, in the VotingEscrow contract, sequentially call the functions:  
 
 ```sh
-commit_smart_wallet_checker(newSmartWalletWhitelistAddress);  
-apply_smart_wallet_checker();  
+function commit_smart_wallet_checker(address newSmartWalletWhitelistAddress);  
+function apply_smart_wallet_checker();  
 ```  
 where `newSmartWalletWhitelistAddress` is the address of your `SmartWalletWhitelist` contract.
 
+## Whitelist access  
+You can specify one particular allowed contract address (or list of addresses) by using following functions:
+```sh
+function approveWallet(address _wallet) external;
+
+function approveWalletList(address[] calldata _wallets) external;
+```
+where `_wallet` (or `_wallets`) - The address of a contract to be approved.  
+
 
 ## Access for All Smart Contracts  
+If you want to allow access to `VotingEscrow` for all smart contracts, you need to set such allowance in the `SmartWalletWhitelist` contract:  
+```sh
+function setAllowAll(bool _isAllowAll) external;
+```
+where `_isAllowAll` - The boolean parameter to set if it allowed for all contract or not.  
 
-If you want to allow access to `VotingEscrow` for all smart contracts, you need to additionally deploy `SmartCheckerAllowAll`.
+
+## Complex configuration  
+If you want to configure access to `VotingEscrow` for smart contracts in more complex way, you can implement desired logic in the separate contract. You can use `SmartChecker` contract as a template. When your implementation is ready you can deploy it:  
 
 ```sh
-npx hardhat run ./scripts/utils/deploySmartAllower.ts --network networkName
+npx hardhat run ./scripts/utils/deploySmartChecker.ts --network networkName
 ```
 
 Then, in the SmartWalletWhitelist contract, call the function:
+```sh
+function setChecker(address SmartCheckerAddress) external;
 ```
-setChecker(smartCheckerAllowAllAddress)
-```
-where `smartCheckerAllowAllAddress` is the address of your `SmartCheckerAllowAll` contract.
+where `SmartCheckerAddress` is the address of your `SmartChecker` contract.
 
-Now any contract can interact with your `VotingEscrow`.
+
